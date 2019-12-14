7ed539c69bb33fa32662a52b94729a2b1a9add1c
linepon <linepon@gmail.com>
2019-12-14 22:03:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function LengthConverterK2M(valNum) {
+    document.getElementById("outputMiles").innerHTML=valNum*0.62137;
+  }
+  
+function LengthConverterM2K(valNum) {
+    document.getElementById("outputKilometers").innerHTML=valNum/0.62137;
+  }
+  
